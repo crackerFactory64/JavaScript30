891172a8d412a176359131904d28dd34b956a5bf
crackerFactory <lee.mander@gmail.com>
2022-10-07 11:41:20 +0100

    timer complete

--- 29 - Countdown Timer/scripts.js
+++ 29 - Countdown Timer/scripts.js
@@ -10,47 +10,40 @@ function setTimer(secs) {
   let seconds = +secs;
   time = new Date(Date.now() + seconds * 1000);
 
-  let remainingMinutes = (time - new Date()) / 60000;
-  if (remainingMinutes < 1) {
-    remainingMinutes = "00";
-  } else if (remainingMinutes < 10) {
-    remainingMinutes = `0${remainingMinutes}`;
-  }
-
-  let remainingSeconds = seconds;
-
   const hours = time.getHours();
   let minutes = time.getMinutes();
   if (minutes < 10) minutes = `0${minutes}`;
-
-  display.querySelector(
-    "h1"
-  ).innerText = `${remainingMinutes}:${remainingSeconds}`;
   display.querySelector("p").innerText = `Be back at ${hours}:${minutes}`;
 
   timerSet = true;
+  updateTimer();
 }
 
 function updateTimer() {
   const difference = time - new Date();
 
-  let minutesRemaing = Math.floor(
-    (difference % (1000 * 60 * 60)) / (1000 * 60)
-  );
+  let minutesRemaing = Math.floor(difference / 60000);
 
-  if (minutesRemaing < 1) {
-    minutesRemaing = "00";
-  } else if (minutesRemaing < 10) {
+  if (minutesRemaing < 10) {
     minutesRemaing = `0${minutesRemaing}`;
   }
 
-  let secondsRemaing = Math.floor((difference % (1000 * 60)) / 1000);
+  let secondsRemaining = Math.floor((difference % (1000 * 60)) / 1000);
 
-  if (secondsRemaing < 1) {
+  if (secondsRemaining < 10) {
+    secondsRemaining = `0${secondsRemaining}`;
+  }
+
+  if (difference < 0) {
+    minutesRemaing = "00";
+    secondsRemaining = "00";
     timerSet = false;
   }
 
-  display.querySelector("h1").innerText = `${minutesRemaing}:${secondsRemaing}`;
+  display.querySelector(
+    "h1"
+  ).innerText = `${minutesRemaing}:${secondsRemaining}`;
+  document.title = `${minutesRemaing}:${secondsRemaining}`;
 }
 
 setInterval(() => {
